cf1e03b66f1bf68ddcb695e7900042e50efd0856
anthoni portocarrero rodriguez <61163166+anthoni-gmu@users.noreply.github.com>
2022-03-28 16:26:48 -0500

    update cart localStore

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,18 +10,23 @@ import { Props } from '../../../types/type';
 import { check_authenticated, load_user, refresh } from '../../redux/actions/auth';
 import {
   get_items,
+  synch_cart
 } from '../../redux/actions/cart';
 
 
 const Layout: React.FC<Props> = ({ title, content, children }) => {
   const dispatch = useDispatch();
   const isAuthenticated = useSelector((state: any) => state.Auth.isAuthenticated)
+  const items = useSelector((state: any) => state.Cart.items)
 
   useEffect(() => {
     if (dispatch && dispatch !== null && dispatch !== undefined) {
       dispatch(check_authenticated());
       dispatch(load_user());
       dispatch(refresh());
+      if(isAuthenticated&&items!==null &&items!==[]){
+        dispatch(synch_cart());
+      }
       dispatch(get_items());
 
     }
